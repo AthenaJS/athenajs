8cbceea0745decf3cfff7c2b41e3c36e0442645f
warpdesign <nicolas.ramz@gmail.com>
2017-08-06 13:34:53 +0200

    ADDED: Object.notify() as a wrapper for NotificationManager

--- js/Notification/NotificationManager.js
+++ js/Notification/NotificationManager.js
@@ -1,28 +1,32 @@
 
-    /*jshint devel: true, bitwise: false*/
-    var eventCallbacks = {};
+/*jshint devel: true, bitwise: false*/
+var eventCallbacks = {};
 
-    export default {
-        notify: function(eventType, data) {
-            // console.log('[NM] got event', eventType, 'with data', data);
-            const params = {type: eventType, data: data};
+const NotificationManager = {
+    notify: function (eventType, data) {
+        // console.log('[NM] got event', eventType, 'with data', data);
+        const params = { type: eventType, data: data };
 
-            if (eventCallbacks[eventType]) {
-                eventCallbacks[eventType].forEach((callback) => callback(params));
-            } else if (eventCallbacks['*']) {
-                eventCallbacks['*'].forEach((callback) => callback(params));
-            }
-        },
-        listen: function(eventType, method) {
-            console.log('[NM] listening to event', eventType);
-            let eventList = eventType.replace(/\s+/g, ' ').split(' ');
-
-            eventList.forEach((eventType) => {
-                if (!eventCallbacks[eventType]) {
-                    eventCallbacks[eventType] = [];
-                }
-
-                eventCallbacks[eventType].push(method);
-            });
+        if (eventCallbacks[eventType]) {
+            eventCallbacks[eventType].forEach((callback) => callback(params));
+        } else if (eventCallbacks['*']) {
+            eventCallbacks['*'].forEach((callback) => callback(params));
         }
-    };
+    },
+    listen: function (eventType, method) {
+        console.log('[NM] listening to event', eventType);
+        let eventList = eventType.replace(/\s+/g, ' ').split(' ');
+
+        eventList.forEach((eventType) => {
+            if (!eventCallbacks[eventType]) {
+                eventCallbacks[eventType] = [];
+            }
+
+            eventCallbacks[eventType].push(method);
+        });
+    }
+};
+
+window.NM = NotificationManager;
+
+export default NotificationManager;
