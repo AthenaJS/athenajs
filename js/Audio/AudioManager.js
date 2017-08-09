f06044f2e3e65b8713bb3a101b7b44d2f0233449
warpdesign <nicolas.ramz@gmail.com>
2017-08-09 22:34:23 +0200

    FIXED: Audio resources weren't loaded if game.options.sound wasn't explicitely set to true FIXED: Call console.warn if a sound could not be played

--- js/Audio/AudioManager.js
+++ js/Audio/AudioManager.js
@@ -1,98 +1,94 @@
-       /*jshint devel: true*/
-        /**
-         * `AudioManager` handles playback of audio files loaded using the `ResourceManager`
-         * 
-         * @property {Object} audioCache An hash that stores in-use sounds.
-         * The key is the id of the sound.
-         * @property {boolean} enabled This is set to false when sound playback is disabled.
-         */
-        export default {
-            audioCache: {},
-            enabled: true,
-            /**
-             * Adds a new sound element to the audio cache.
-             * *Note* if a sound with the same id has already been added, it will be replaced
-             * by the new one.
-             * 
-             * @param {String} id
-             * @param {HTMLAudioElement} element
-             */
-            addSound: function (id, element) {
-                this.audioCache[id] = element;
-            },
-            /**
-             * Toggles global sound playback
-             * 
-             * @param {Boolean} bool whether to enabled or disable sound playback.
-             */
-            toggleSound: function (bool) {
-                this.enabled = bool;
-            },
-            /**
-             * Plays the specified sound with `id`.
-             * 
-             * @param {String} id
-             * @param {Boolean} loop
-             * @param {Float} volume
-             * @param {Float} panning
-             * @returns the created sound instance
-             */
-            play: function (id, loop, volume, panning) {
-                let instance = null,
-                    sound = null;
+/*jshint devel: true*/
+/**
+ * `AudioManager` handles playback of audio files loaded using the `ResourceManager`
+ * 
+ * @property {Object} audioCache An hash that stores in-use sounds.
+ * The key is the id of the sound.
+ * @property {boolean} enabled This is set to false when sound playback is disabled.
+ */
+export default {
+    audioCache: {},
+    enabled: true,
+    /**
+     * Adds a new sound element to the audio cache.
+     * *Note* if a sound with the same id has already been added, it will be replaced
+     * by the new one.
+     * 
+     * @param {String} id
+     * @param {HTMLAudioElement} element
+     */
+    addSound: function (id, element) {
+        this.audioCache[id] = element;
+    },
+    /**
+     * Toggles global sound playback
+     * 
+     * @param {Boolean} bool whether to enabled or disable sound playback.
+     */
+    toggleSound: function (bool) {
+        this.enabled = bool;
+    },
+    /**
+     * Plays the specified sound with `id`.
+     * 
+     * @param {String} id
+     * @param {Boolean} loop
+     * @param {Float} volume
+     * @param {Float} panning
+     * @returns the created sound instance
+     */
+    play: function (id, loop, volume, panning) {
+        let instance = null,
+            sound = null;
 
-                if (!this.enabled) {
-                    return;
-                }
+        if (!this.enabled) {
+            return;
+        }
 
-                try {
-                    sound = this.audioCache[id];
-                } catch (err) {
-                    console.warn('[AM] WARN: unable to play sound', id);
-                    return;
-                }
+        sound = this.audioCache[id];
 
-                if (typeof sound === 'undefined') {
-                    return;
-                }
+        if (typeof sound === 'undefined') {
+            console.warn('[AM] could not find sound, did you load:', id);
+            return;
+        }
 
-                if (typeof sound.loop === 'function') {
-                    sound.loop(loop || false);
-                } else {
-                    sound.loop = loop || false;
-                }
+        if (typeof sound.loop === 'function') {
+            sound.loop(loop || false);
+        } else {
+            sound.loop = loop || false;
+        }
 
-                instance = sound.play({
-                    panning: [panning || 0, 0, 5],
-                    volume: volume || 1,
-                    loop: loop || false
-                });
+        instance = sound.play({
+            panning: [panning || 0, 0, 5],
+            volume: volume || 1,
+            loop: loop || false
+        });
 
-                return instance;
-            },
-            /**
-             * Stops playing the sound id
-             * 
-             * @param {any} id
-             * @param {any} instanceId
-             * @returns
-             */
-            stop: function (id, instanceId) {
-                let sound = null;
+        return instance;
+    },
+    /**
+     * Stops playing the sound id
+     * 
+     * @param {any} id
+     * @param {any} instanceId
+     * @returns
+     */
+    stop: function (id, instanceId) {
+        let sound = null;
 
-                if (!this.enabled) {
-                    return;
-                }
+        if (!this.enabled) {
+            return;
+        }
 
-                try {
-                    sound = this.audioCache[id];
-                } catch (err) {
-                    console.warn('[AM] WARN: unable to stop sound', id);
-                    return;
-                }
+        try {
+            sound = this.audioCache[id];
+        } catch (err) {
+            console.warn('[AM] WARN: unable to stop sound', id);
+            return;
+        }
 
-                if (sound && typeof sound.stop === 'function') {
-                    sound.stop(instanceId || undefined);
-                }
-            }
-        };
+        if (sound && typeof sound.stop === 'function') {
+            sound.stop(instanceId || undefined);
+        }
+    }
+};
