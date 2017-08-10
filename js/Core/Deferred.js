00e99d4b55631ee63cab0ec933eccd4ea7b834d1
warpdesign <nicolas.ramz@gmail.com>
2017-08-10 11:16:45 +0200

    ADDED: Deferred.resolve as shortcut to Promise.resolve

--- js/Core/Deferred.js
+++ js/Core/Deferred.js
@@ -1,13 +1,20 @@
-import Promise from 'es6-promise';
+import Promise from "es6-promise";
 
+console.log("deferred");
 /**
  * Simple wrapper for ES6 native Promise
  */
 export default class Deferred {
-    constructor() {
-        this.promise = new Promise((resolve, reject) => {
-            this.resolve = resolve;
-            this.reject = reject;
-        });
-    }
+  constructor() {
+    this.promise = new Promise((resolve, reject) => {
+      this.resolve = resolve;
+      this.reject = reject;
+    });
+  }
+
+  static resolve(val) {
+    return Promise.resolve(val);
+  }
 }
+
+console.log("end deferred");
