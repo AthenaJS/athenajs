d3e59293194f0aac39d6b3fcceb4eb3fd33ccc46
warpdesign <nicolas.ramz@gmail.com>
2017-12-07 09:07:33 +0100

    ADDED: missing jsDoc for Wave & Tile

--- js/Drawable/Wave.js
+++ js/Drawable/Wave.js
@@ -2,15 +2,35 @@ import RM from '../Resource/ResourceManager';
 
 /**
  * This class allows to handle wave of Drawables
+ *
+ * In AthenaJS, waves of enemies can be triggered by certain action onto the map.
+ *
+ * Once every enemies of a wave have been destroyed, an action can be triggered,
+ * for eg. to drop rewards onto the map.
  */
 export default class Wave {
+    /**
+     * Creates a new Wave
+     *
+     * @param {Object} options
+     * @param {Number} options.size The size of the Wave.
+     * @param {String} options.type The type of wave, ie. what will happen after the wave have been destroyed.
+     * @param {Object} options.afterDestroyData The data needed for the `type` trigger.
+     */
     constructor(options) {
         this.counter = options.size;
         this.type = options.afterDestroy;
         this.data = options.afterDestroyData;
-        this.delay = options.afterDestroyDelay || 0;
+        // this.delay = options.afterDestroyDelay || 0;
     }
 
+    /**
+     * Removes an element from the wave.
+     *
+     * This method gets called once the drawable's `destroy` method is called
+     *
+     * @param {Drawable} element The element that was removed.
+     */
     remove(element) {
         this.counter--;
 
@@ -19,6 +39,14 @@ export default class Wave {
         }
     }
 
+    /**
+     * Generates the sprite's drawable options, because some parameters, like position
+     * may depend on the wave element's positions
+     *
+     * @param {Drawable} element The element to use as a base
+     *
+     * @returns {Object} The options to pass to the drawable constructor
+     */
     getSpriteOptions(element) {
         const options = Object.assign({}, this.data.spriteOptions);
 
@@ -33,6 +61,13 @@ export default class Wave {
         return options;
     }
 
+    /**
+     * Called when the last element of a wave have been destroyed.
+     *
+     * This destroys the wave itself, triggering an option event
+     *
+     * @param {Drawable} element The last Drawable that was destroyed and triggered the wave destroy.
+     */
     destroy(element) {
         switch (this.type) {
             case 'reward':
--- js/Map/Tile.js
+++ js/Map/Tile.js
@@ -1,16 +1,32 @@
+/**
+ * Class that describes a tile
+ */
 class Tile {
+    /**
+     * Creates a new Tile
+     *
+     * @param {Object} options
+     * @param {Number} [options.offsetX=0] The horizontal offset of the tile in the tilesheet.
+     * @param {Number} [options.offsetY=0] The vertical offset of the tile in the tilesheet.
+     * @param {Number} [options.width=16] The tile width in pixels.
+     * @param {Number} [options.height=16] The tile height in pixels.
+     */
     constructor(options) {
         this.offsetX = options.offsetX || 0;
         this.offsetY = options.offsetY || 0;
-        this.width = options.width || 0;
-        this.height = options.height || 0;
+        this.width = options.width || 16;
+        this.height = options.height || 16;
 
-        // move
+        // TODO: this is not used yet but could allow to change inertia based on the tile on which the
+        // player is moving
         this.inertia = options.inertia || 1;            // this is the inertia factor
         this.upCollide = options.upCollide || true;     // will object collide when dropping over this tile ?
         this.downCollide = options.downCollide || true;
     }
 
+    /**
+     * Static tile behaviors
+     */
     static get TYPE() {
         return {
             AIR: 1,
